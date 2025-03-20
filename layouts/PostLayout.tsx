dbec8f99f7cba348dd25dcb03fec585e6529f55a
KevinBulme <kevin.bulme@gmail.com>
2025-03-20 17:05:51 +0900

    feat: small CSS tweaks for post layout

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -42,7 +42,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             <div className="space-y-1 text-center">
               <dl className="space-y-10">
                 <div>
-                  <dt className="sr-only">Published on</dt>
+                  <dt className="sr-only">公開日</dt>
                   <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
                     <time dateTime={date}>
                       {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
@@ -57,7 +57,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
           </header>
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0 dark:divide-gray-700">
             <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
-              <dt className="sr-only">Authors</dt>
+              <dt className="sr-only">著者</dt>
               <dd>
                 <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-y-8 xl:space-x-0">
                   {authorDetails.map((author) => (
@@ -73,14 +73,10 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                           />
                         )}
                         <dl className="text-sm leading-5 font-medium whitespace-nowrap">
-                          <dt className="sr-only">Name</dt>
-                          <dd className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                            {author.name}
-                          </dd>
-                          <dt className="sr-only">Occupation</dt>
-                          <dd className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                            {author.occupation}
-                          </dd>
+                          <dt className="sr-only">名前</dt>
+                          <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
+                          <dt className="sr-only">職業</dt>
+                          <dd className="text-gray-500 dark:text-gray-400">{author.occupation}</dd>
                         </dl>
                       </li>
                     </Link>
@@ -104,7 +100,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                 {tags && (
                   <div className="py-4 xl:py-8">
                     <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                      Tags
+                      タグ
                     </h2>
                     <div className="flex flex-wrap">
                       {tags.map((tag) => (
@@ -118,7 +114,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                     {prev && prev.path && (
                       <div>
                         <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                          Previous Article
+                          前の記事
                         </h2>
                         <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                           <Link href={`/${prev.path}`}>{prev.title}</Link>
@@ -128,7 +124,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                     {next && next.path && (
                       <div>
                         <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                          Next Article
+                          次の記事
                         </h2>
                         <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                           <Link href={`/${next.path}`}>{next.title}</Link>
@@ -144,7 +140,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                   className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                   aria-label="Back to the blog"
                 >
-                  &larr; Back to the blog
+                  &larr; ブログ一覧へ戻る
                 </Link>
               </div>
             </footer>
