64a7c3aca49d8a7bde2814f6b02ff194346a86f7
Emire <97857043+EmireHaouas@users.noreply.github.com>
2025-09-11 03:09:44 +0100

    fix:update service page

--- app/[locale]/services/page.tsx
+++ app/[locale]/services/page.tsx
@@ -1,20 +1,17 @@
 import { genPageMetadata } from 'app/seo'
 import ServicesPageClient from './services.page.client'
 
-interface ServicesPageProps {
-  params: { locale: string }
-}
-
 export const generateMetadata = async ({ params }: { params: { locale: string } }) => {
   const { locale } = params
   const messages = (await import(`../../../i18n/messages/${locale}.json`)).default
+
   return genPageMetadata({
     title: messages.services.metaTitle,
     description: messages.services.metaDescription,
   })
 }
 
-export default async function ServicesPage({ params }: ServicesPageProps) {
+export default async function ServicesPage({ params }: { params: { locale: string } }) {
   const { locale } = params
   return <ServicesPageClient locale={locale} />
 }
